c3945d29f893398cb41fc0547511ce02435044c0
Henry Chen <henrychen385@gmail.com>
2019-08-02 23:28:12 +0800

    testing draftjs implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import {Editor, EditorState, RichUtils} from 'draft-js'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {editorState: EditorState.createEmpty()}
+    this.onChange = (editorState) => this.setState({editorState})
+    this.onBoldClick = () => this.onChange(RichUtils.toggleInlineStyle(
+      this.state.editorState, 'BOLD'
+    ))
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Hello World</h1>
+        <div>
+          <button onClick={this.onBoldClick}>Bold</button>
+        </div>
+        <Editor 
+          editorState={this.state.editorState} 
+          onChange={this.onChange}
+        />
+      </div>
+    )
+  }
+
 }
 
 export default App;
