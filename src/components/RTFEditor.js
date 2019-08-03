bb48334b44e0608200f9c08b7c411c0864fb3fe5
Henry Chen <henrychen385@gmail.com>
2019-08-03 08:56:05 +0800

    added new buttons

--- src/components/RTFEditor.js
+++ src/components/RTFEditor.js
@@ -3,6 +3,8 @@ import { Editor, EditorState, RichUtils } from 'draft-js'
 import styled from 'styled-components'
 
 const EditorContainer = styled.div`
+	@import url('https://fonts.googleapis.com/css?family=Nova+Mono&display=swap');
+	font-family: 'Nova Mono', monospace;
 	border:solid 1px #d7dbe0
 	padding:6pt
 	sizing:border-box
@@ -10,6 +12,7 @@ const EditorContainer = styled.div`
 `
 const ControlContainer = styled.div`
 	display:grid
+	grid-template-columns: 48pt 48pt 48pt 48pt
 `
 const StyleButton = styled.button`
 	border:none
@@ -23,13 +26,18 @@ class RTFEditor extends Editor {
 		super(props)
 		this.state = {editorState: EditorState.createEmpty()}
 		this.onChange = (event) => this.setState({editorState: event})
+		this.makeBold = () => this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+		this.makeItalic = () => this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+		this.makeUnderlined = () => this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINED'))
 	}
 	render() {
 		return (
 			<div>
 				<h1>This is the Editor</h1>
 				<ControlContainer>
-					<StyleButton>Bold</StyleButton>
+					<StyleButton onClick={this.makeBold}>Bold</StyleButton>
+					<StyleButton onClick={this.makeItalic}>Italic</StyleButton>
+					<StyleButton onClick={this.makeUnderlined}>Underlined</StyleButton>
 				</ControlContainer>
 				<EditorContainer>
 					<Editor 
